0a5f3b247c54bf61ccb030924a308fc430ae2f20
syennew <syen8351815@gmail.com>
2022-10-12 20:59:30 +0900

    Initial Commit

---
+++ GoToPOM☆/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoToPOM☆
+//
+//  Created by 石丸剣心 on 2022/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
